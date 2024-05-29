bb687a62e4730d9f90d87f80d452233b07ee11b4
MysticMaccc <cosmicsher96@gmail.com>
2024-05-29 15:01:10 +0800

    implemented update of status for each request

--- resources/views/components/td.blade.php
+++ resources/views/components/td.blade.php
@@ -1,5 +1,5 @@
 <td data-tw-merge=""
-    class="px-5 py-3 border-b dark:border-darkmode-300 box rounded-l-none 
+    class="px-5 py-3 border-b dark:border-darkmode-300 box rounded-l-none text-xs
     rounded-r-none border-x-0 text-start shadow-[5px_3px_5px_#00000005] first:rounded-l-[0.6rem] 
     first:border-l last:rounded-r-[0.6rem] last:border-r dark:bg-darkmode-600 
     hover:bg-teal-500 hover:text-stone-50">
